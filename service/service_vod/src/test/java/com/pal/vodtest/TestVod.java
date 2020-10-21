9468013a81a1d32f072e9e2ad19b4d71323d56ab
buptpal <925978482@qq.com>
2020-10-21 16:06:05 +0800

    上传视频测试

--- service/service_vod/src/test/java/com/pal/vodtest/TestVod.java
+++ service/service_vod/src/test/java/com/pal/vodtest/TestVod.java
@@ -1,5 +1,8 @@
 package com.pal.vodtest;
 
+import com.aliyun.vod.upload.impl.UploadVideoImpl;
+import com.aliyun.vod.upload.req.UploadVideoRequest;
+import com.aliyun.vod.upload.resp.UploadVideoResponse;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.vod.model.v20170321.GetPlayInfoRequest;
 import com.aliyuncs.vod.model.v20170321.GetPlayInfoResponse;
@@ -15,7 +18,30 @@ import java.util.List;
  */
 public class TestVod {
     public static void main(String[] args) throws Exception {
-        getPlayAuth();
+        String accessKeyId = "LTAI4GANiAjUgRPjAAzT1n8a";
+        String accessKeySecret = "P8HIOB1bc3ibNOJL8UwfASriKOnX19";
+        String title = "雪景";
+        String fileName = "D:\\桌面\\video.mp4";
+
+
+        // 上传视频的方法
+        UploadVideoRequest request = new UploadVideoRequest(accessKeyId, accessKeySecret, title, fileName);
+        /* 可指定分片上传时每个分片的大小，默认为2M字节 */
+        request.setPartSize(2 * 1024 * 1024L);
+        /* 可指定分片上传时的并发线程数，默认为1，(注：该配置会占用服务器CPU资源，需根据服务器情况指定）*/
+        request.setTaskNum(1);
+
+        UploadVideoImpl uploader = new UploadVideoImpl();
+        UploadVideoResponse response = uploader.uploadVideo(request);
+        System.out.print("RequestId=" + response.getRequestId() + "\n");  //请求视频点播服务的请求ID
+        if (response.isSuccess()) {
+            System.out.print("VideoId=" + response.getVideoId() + "\n");
+        } else {
+            /* 如果设置回调URL无效，不影响视频上传，可以返回VideoId同时会返回错误码。其他情况上传失败时，VideoId为空，此时需要根据返回错误码分析具体错误原因 */
+            System.out.print("VideoId=" + response.getVideoId() + "\n");
+            System.out.print("ErrorCode=" + response.getCode() + "\n");
+            System.out.print("ErrorMessage=" + response.getMessage() + "\n");
+        }
     }
     // 根据视频id获取视频播放地址
     public static void getPlayUrl () throws Exception {
