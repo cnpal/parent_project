8044d48e149bd3d4d6b735408e183015d6a79dde
buptpal <925978482@qq.com>
2020-10-25 12:48:27 +0800

    整合nacos

--- service/service_edu/src/main/java/com/pal/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/pal/eduservice/EduApplication.java
@@ -2,6 +2,7 @@ package com.pal.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.pal"})
+@EnableDiscoveryClient
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class,args);
